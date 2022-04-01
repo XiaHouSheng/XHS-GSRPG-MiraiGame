19d8230990660eacd7db28d1ca8503f72728ece8
XiaHouSheng <1754276102@qq.com>
2022-04-01 23:47:07 +0800

    消息解析|监听完善

--- MiraiS/bot.py
+++ MiraiS/bot.py
@@ -2,42 +2,53 @@ import asyncio
 import websockets
 import requests
 import json
-
+import Message
 
 class bot:
     def __init__(self):
         session = json.loads(requests.post(url="http://127.0.0.1:5700/verify", json={"verifyKey": "mzxclhml10"}).text)[
             "session"]
         requests.get("http://127.0.0.1:5700/bind", json={"sessionKey": session, "qq": 213303398})
+        self._taskes = {}
 
-    async def OnMsg(self,agrs):
+    async def OnMsg(self,func):
         async with websockets.connect("ws://localhost:5701/message?verifyKey=mzxclhml10&qq=213303398") as websocket:
-            self.session=json.loads(await websocket.recv())["data"]["session"]
+            self.session = json.loads(await websocket.recv())["data"]["session"]
             while True:
                 recv_text = await websocket.recv()
-                for i in agrs:
-                    await i(recv_text)
+                MessageObj = Message.Messgae(data=recv_text)
+                print(MessageObj.__str__())
+                if MessageObj.messageType=="GroupMessage":
+                    await func["GroupMsg"](MessageObj)
+                elif MessageObj.messageType=="FriendMessage":
+                    await func["FriendMsg"](MessageObj)
 
-    async def OnEvent(self,agrs):
+
+    async def OnEvent(self):
         async with websockets.connect("ws://localhost:5701/event?verifyKey=mzxclhml10&qq=213303398") as websocket:
             self.session = json.loads(await websocket.recv())["data"]["session"]
             while True:
                 recv_text = await websocket.recv()
-                for i in agrs:
-                    await i(recv_text)
+                #Event~~~
 
-    def runServer(self,*args):
-        asyncio.get_event_loop().run_until_complete(self.OnMsg(args))
+    def register(self,text):
+        def wrapper(func):
+            self._taskes[text]=func
+        return wrapper
 
+    def runServer(self):
+        asyncio.get_event_loop().run_until_complete(self.OnMsg(self._taskes))
 
 
 bot = bot()
 
 
+@bot.register("GroupMsg")
 async def onMsg(data):
-    print("message", data)
+    pass
 
+@bot.register("FriendMsg")
+async def onMsg(data):
+    pass
 
-
-
-bot.runServer(onMsg)
+bot.runServer()
